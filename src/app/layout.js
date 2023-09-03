bbaa225f3f68f532d2da05eededf86aa96a7d5f4
ricardo.machado <ricardo.machado@paygo-consult.com.br>
2023-09-03 15:29:38 -0300

    theme dark

--- src/app/layout.js
+++ src/app/layout.js
@@ -56,10 +56,10 @@ export default function RootLayout({ children }) {
         <AuthProvider>
           <SettingsProvider
             defaultSettings={{
-              themeMode: 'light', // 'light' | 'dark'
+              themeMode: 'dark', // 'light' | 'dark'
               themeDirection: 'ltr', //  'rtl' | 'ltr'
               themeContrast: 'default', // 'default' | 'bold'
-              themeLayout: 'vertical', // 'vertical' | 'horizontal' | 'mini'
+              themeLayout: 'mini', // 'vertical' | 'horizontal' | 'mini'
               themeColorPresets: 'default', // 'default' | 'cyan' | 'purple' | 'blue' | 'orange' | 'red'
               themeStretch: false,
             }}
