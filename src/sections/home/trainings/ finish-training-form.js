23c49cb9ff60cdaf1d785ce212865c5be66c1d4f
Ricardo Matta <ricardommps@gmail.com>
2023-12-18 14:08:07 -0300

    improvements to the end training form

--- src/sections/home/trainings/ finish-training-form.js
+++ src/sections/home/trainings/ finish-training-form.js
@@ -4,6 +4,7 @@ import Alert from '@mui/material/Alert';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import { useTheme } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import { useCallback, useEffect, useMemo } from 'react';
 import { useForm } from 'react-hook-form';
@@ -14,6 +15,7 @@ import * as Yup from 'yup';
 
 import RPSSlider from './rpe-slider';
 export default function FinishTrainingForm({ trainingId, onClose }) {
+  const theme = useTheme();
   const { onFinishedTraining, finishedtrainingDetailStatus } = useHome();
 
   const NewTrainingSchema = Yup.object().shape({
@@ -89,6 +91,31 @@ export default function FinishTrainingForm({ trainingId, onClose }) {
     </>
   );
 
+  const paceInfo = (
+    <>
+      <Stack flexDirection={'row'}>
+        <Typography sx={{ marginRight: 2, color: theme.palette.warning.dark }}>
+          Separadores válidos
+        </Typography>
+        <Typography sx={{ fontWeight: 'bold', marginRight: 1, color: theme.palette.warning.dark }}>
+          {' '}
+          .
+        </Typography>
+        <Typography sx={{ fontWeight: 'bold', marginRight: 1, color: theme.palette.warning.dark }}>
+          {' '}
+          ,
+        </Typography>
+      </Stack>
+    </>
+  );
+
+  function toHoursAndMinutes(totalMinutes) {
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = totalMinutes % 60;
+
+    return `${hours}h${minutes > 0 ? ` ${minutes}m` : ''}`;
+  }
+
   useEffect(() => {
     getTrimp();
   }, [values.duration, values.rpe]);
@@ -102,27 +129,29 @@ export default function FinishTrainingForm({ trainingId, onClose }) {
               <RHFTextField
                 name="distance"
                 label="Distância em metros *"
-                variant="standard"
+                variant="outlined"
                 type="number"
                 helperText={`${Number(values.distance) / 1000} km`}
               />
               <RHFTextField
                 name="duration"
                 label="Tempo total em minutos *"
-                variant="standard"
+                variant="outlined"
                 type="number"
+                helperText={toHoursAndMinutes(Number(values.duration))}
               />
               <RHFTextField
                 name="pace"
                 label="Pace médio da sessão"
-                variant="standard"
+                variant="outlined"
                 type="number"
+                helperText={paceInfo}
               />
+              <RHFTextField name="link" label="Link" variant="outlined" />
               <RPSSlider control={control} />
               <Stack>
                 <Typography>{`Trimp: ${values.trimp}`}</Typography>
               </Stack>
-              <RHFTextField name="link" label="Link" variant="standard" />
               <RHFTextField name="comments" label="Comentários" multiline rows={6} />
             </Box>
             <Stack pt={2} sx={{ width: '100%' }} spacing={2}>
