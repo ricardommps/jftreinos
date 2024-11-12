9e8f0a2cc3f648d48665983cb924a4b44a97d7a1
Ricardo Matta <ricardommps@gmail.com>
2024-11-12 12:48:08 -0300

    Adiciona legenda para biset, triset ou circuito

--- src/sections/home/trainings/workout/workout-view-group.js
+++ src/sections/home/trainings/workout/workout-view-group.js
@@ -1,3 +1,5 @@
+import Box from '@mui/material/Box';
+import Chip from '@mui/material/Chip';
 import Paper from '@mui/material/Paper';
 import { alpha } from '@mui/material/styles';
 import React from 'react';
@@ -5,6 +7,17 @@ import React from 'react';
 import WorkoutViewGroupItem from './workout-view-group-item';
 
 export default function WorkoutViewGroup({ media, exerciseInfo, workoutLoad }) {
+  const handleType = () => {
+    if (media.length === 2) {
+      return 'BISET';
+    }
+
+    if (media.length === 3) {
+      return 'TRISET';
+    }
+
+    return 'CIRCUITO';
+  };
   return (
     <>
       <Paper
@@ -21,6 +34,9 @@ export default function WorkoutViewGroup({ media, exerciseInfo, workoutLoad }) {
           bgcolor: (theme) => alpha(theme.palette.grey[500], 0.04),
         }}
       >
+        <Box justifyContent="flex-start" display="flex" p={1} gap={2}>
+          <Chip label={handleType()} color="info" />
+        </Box>
         {media.map((subMedia, index) => (
           <React.Fragment key={index}>
             <WorkoutViewGroupItem
