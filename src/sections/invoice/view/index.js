d106cee02f20888e42a9316571c4418da9b8dbad
Ricardo Matta <ricardommps@gmail.com>
2025-02-16 18:34:32 -0300

    correção de lint

--- src/sections/invoice/view/index.js
+++ src/sections/invoice/view/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 // eslint-disable-next-line simple-import-sort/exports
 export { default as InvoiceDownload } from './invoice-download';
 export { default as InvoiceView } from './invoice.view';
