10b8eb6f5ffad3c24fff95843f6784d00daa47b5
Ricardo Matta <ricardommps@gmail.com>
2025-04-13 09:54:57 -0300

    correção de data

--- src/utils/format-time.js
+++ src/utils/format-time.js
@@ -53,10 +53,10 @@ export function convertDate(inputDate) {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  const seconds = String(date.getSeconds()).padStart(2, '0');
-  const milliseconds = String(date.getMilliseconds()).padStart(6, '0');
+  const hours = '12';
+  const minutes = '00';
+  const seconds = '00';
+  const milliseconds = '00';
 
   return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
 }
