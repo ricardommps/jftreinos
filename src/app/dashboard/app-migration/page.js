d9a1575c61aaa5b0107633ede2018b00a035839b
Ricardo Matta <ricardommps@gmail.com>
2025-08-27 16:33:44 -0300

    Ajustes para o app

---
+++ src/app/dashboard/app-migration/page.js
@@ -0,0 +1,12 @@
+// sections
+import { AppMigration } from 'src/sections/app-migration/view';
+
+// ----------------------------------------------------------------------
+
+export const metadata = {
+  title: 'Foltz: Novo app',
+};
+
+export default function Page() {
+  return <AppMigration />;
+}
